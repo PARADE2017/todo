aa24d87eb319ce8972050f4326821f5a3e02dd35
Teresa McClintic <tmcclintic@gmail.com>
2018-04-22 16:51:30 +0000

    add task

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -27,4 +27,15 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "tasks#create" do
+    it "should allow new task to be created" do
+      post :create, params: {task: {title: "fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("fix things")
+      expect(Task.last.title).to eq("fix things")
+    end
+
+  end
+
 end
